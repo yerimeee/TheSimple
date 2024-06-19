22786f47e4097eeffa6548f76e1418ca55851a71
yerimeee <102905442+yerimeee@users.noreply.github.com>
2024-06-19 23:00:00 +0900

    판매 페이지 UI 완성

--- app/src/main/java/com/interview/task/MainActivity_sell.kt
+++ app/src/main/java/com/interview/task/MainActivity_sell.kt
@@ -2,12 +2,12 @@ package com.interview.task
 
 import android.os.Build
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -21,10 +21,28 @@ class MainActivity_sell : AppCompatActivity() {
         val localDateTime: LocalDateTime = LocalDateTime.now()
         val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")
         val watch = localDateTime.format(formatter)
-
         val t_watch: TextView = findViewById(R.id.t_watch)
         t_watch.text = watch
-        
+
+        // 버튼에 텍스트 중복으로 넣기
+        val btn_menu1: Button = findViewById(R.id.btn_menu1)
+        val btn_menu2: Button = findViewById(R.id.btn_menu2)
+        val btn_menu3: Button = findViewById(R.id.btn_menu3)
+        val btn_menu4: Button = findViewById(R.id.btn_menu4)
+        val btn_menu5: Button = findViewById(R.id.btn_menu5)
+
+        val t_btn1 = SpannableStringBuilder("제육덮밥\n\n9000")
+        val t_btn2 = SpannableStringBuilder("라면\n\n3500")
+        val t_btn3 = SpannableStringBuilder("김밥\n\n2900")
+        val t_btn4 = SpannableStringBuilder("볶음밥\n\n8000")
+        val t_btn5 = SpannableStringBuilder("육개장\n\n9000")
+
+        btn_menu1.text = t_btn1
+        btn_menu2.text = t_btn2
+        btn_menu3.text = t_btn3
+        btn_menu4.text = t_btn4
+        btn_menu5.text = t_btn5
+
 
     }
 }
